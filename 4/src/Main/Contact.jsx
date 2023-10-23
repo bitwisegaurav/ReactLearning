355e336c3a29e6909110ae4fecc05bcf3a8cb2e8
Gaurav Mishra <gauravmishra2401@gmail.com>
2023-10-23 14:16:20 +0530

    updated design of contact component

--- 4/src/Main/Contact.jsx
+++ 4/src/Main/Contact.jsx
@@ -9,30 +9,30 @@ export default function Contact() {
       <div>
         <label htmlFor="first-name" className="block text-sm font-semibold leading-6">First name</label>
         <div className="mt-2.5">
-          <input type="text" name="first-name" id="first-name" autoComplete="given-name" className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm bg-slate-200 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          <input type="text" name="first-name" id="first-name" autoComplete="given-name" className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm bg-slate-200 text-slate-800 focus:ring-indigo-600 sm:text-sm sm:leading-6" />
         </div>
       </div>
       <div>
         <label htmlFor="last-name" className="block text-sm font-semibold leading-6">Last name</label>
         <div className="mt-2.5">
-          <input type="text" name="last-name" id="last-name" autoComplete="family-name" className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm  bg-slate-200 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          <input type="text" name="last-name" id="last-name" autoComplete="family-name" className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm  bg-slate-200 text-slate-800 focus:ring-indigo-600 sm:text-sm sm:leading-6" />
         </div>
       </div>
       <div className="sm:col-span-2">
         <label htmlFor="email" className="block text-sm font-semibold leading-6">Email</label>
         <div className="mt-2.5">
-          <input type="email" name="email" id="email" autoComplete="email" className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm  bg-slate-200 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+          <input type="email" name="email" id="email" autoComplete="email" className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm  bg-slate-200 text-slate-800 focus:ring-indigo-600 sm:text-sm sm:leading-6" />
         </div>
       </div>
       <div className="sm:col-span-2">
         <label htmlFor="message" className="block text-sm font-semibold leading-6">Message</label>
         <div className="mt-2.5">
-          <textarea name="message" id="message" rows="4" className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm  placeholder:text-gray-400 bg-slate-200 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+          <textarea name="message" id="message" rows="4" className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm text-slate-800 bg-slate-200 focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
         </div>
       </div>
     </div>
     <div className="mt-10">
-      <button type="submit" className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let's talk</button>
+      <button type="submit" className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer">Lets talk</button>
     </div>
   </form>
 </div>
