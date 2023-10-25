62b3985131b96319349be64fe78d6e999f5040f5
Gaurav Mishra <gauravmishra2401@gmail.com>
2023-10-25 19:37:35 +0530

    removed the css code and used tailwind for design

--- 1_useState/src/App.jsx
+++ 1_useState/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import './App.css'
 function App() {
-  const colors = ["#F1F1F1","#FFA07A","#FFD700","#FF69B4","#FF6347","#FF4500","#FF8C00","#FFA500","#FFDAB9","#FFE4E1","#FFE4B5","#FFEFD5","#FFF0F5","#FFF8DC","#FFFACD","#FFFAF0","#FFFF00","#FFFFE0","#FFFFF0","#FFC0CB"];
+  const colors = ["#475569","#FFA07A","#FFD700","#FF69B4","#FF6347","#FF4500","#FF8C00","#FFA500","#FFDAB9","#FFE4E1","#FFE4B5","#FFEFD5","#FFF0F5","#FFF8DC","#FFFACD","#FFFAF0","#FFFF00","#FFFFE0","#FFFFF0","#FFC0CB"];
 
   const [count, setCount] = useState(0);
   const [colorCount, setColorCount] = useState(0);
@@ -20,14 +19,14 @@ function App() {
 
   return (
     <>
-    <div className="container" style={{background: colors[colorCount]}}>
-      <div className="card">
-        <p className='val'>Value {count}</p>
-        <div className="btns">
-        <button onClick={inc} style={{marginRight: "20px"}}>Inc</button> 
-        <button onClick={dec}>Dec</button>
+    <div className="grid place-items-center h-screen w-screen text-xl" style={{background: colors[colorCount]}}>
+      <div className="grid bg-slate-800 gap-8 text-white px-8 py-6 rounded-lg">
+        <p className='bg-gray-300 px-5 py-3 rounded-md text-slate-800 text-center'>Value : {count}</p>
+        <div className="flex gap-6">
+          <button onClick={inc} className="bg-slate-700 px-7 py-3 rounded-lg hover:bg-slate-600">Increase</button> 
+          <button onClick={dec} className="bg-slate-700 px-7 py-3 rounded-lg hover:bg-slate-600">Decrease</button>
         </div>
-        <button onClick={change}>Change color</button>
+        <button onClick={change} className="bg-blue-700 px-5 py-3 rounded-lg hover:bg-blue-800">Change color</button>
       </div>
     </div>
     </>
